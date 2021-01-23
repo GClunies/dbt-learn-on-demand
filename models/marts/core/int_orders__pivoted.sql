e1cafec28b67d26c73876356da3ada7e5a7318e8
Greg Clunies <gclunies@surfline.com>
2021-01-23 20:29:27 +0000

    make pivot model using jinja!

--- models/marts/core/int_orders__pivoted.sql
+++ models/marts/core/int_orders__pivoted.sql
@@ -1,3 +1,6 @@
+{%- set payment_methods = ['bank_transfer', 'coupon', 'credit_card', 'gift_card'] -%}
+
+
 with payments as (
 
     select * from {{ ref('stg_payments') }}
@@ -8,14 +11,13 @@ pivoted as (
 
     select
         order_id,
-
-        sum(
-            case
-                when payment_method = 'bank trasfer' then amount
-                else 0
-            end
-        ) as bank_transfer_amount
         
+        {% for payment_method in payment_methods -%}
+        sum(case when payment_method = '{{ payment_method }}' then amount else 0 end) as {{ payment_method }}_amount
+        {%- if not loop.last -%}
+            ,
+        {%- endif %}
+        {% endfor %}
     from payments
 
     where status = 'success'
